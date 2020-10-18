c68a65ab656ba3abdca42a5840b6b2a244f03b31
Leslie Le <lel23@mail.wlu.edu>
2020-10-17 20:51:26 -0400

    sklearn model

--- linear_regression.py
+++ linear_regression.py
@@ -4,6 +4,7 @@ from mlxtend.plotting import scatterplotmatrix
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
@@ -12,66 +13,38 @@ Name: Leslie Le
 Date: 10/16/2020
 
 linear_regression.py
-
-v1..Linear Regression between LOR and CGPA
-....LOSS: 0.77243114
-....WEIGHT: 1.2640988
-....BIAS: 4.0585027
 """
 
-class LinearRegression():
-    def __init__(self):
-        self.w0 = tf.Variable(np.random.randn(), name='weight')
-        self.bias = tf.Variable(np.random.randn(), name='bias')
-
-    def __call__(self, x):
-        return self.w0 * x + self.bias
-
-def loss(y, pred):
-    return tf.reduce_mean(tf.square(y - pred))
-
-def train(model, x, y, lr=0.006):
-    with tf.GradientTape() as t:
-        modelLoss = loss(y, model(x))
-        change = t.gradient(modelLoss, [model.w0, model.bias])
-    model.w0.assign_sub(change[0]*lr) # change weight with first gradient given
-    model.bias.assign_sub(change[1]*lr) # change bias with second gradient
-
-df = pd.read_csv("Admission_Predict.csv", header=0,
-                 usecols=[5,6,8])
+df = pd.read_csv('Admission_Predict.csv')
 df.dropna(inplace=True)
-print(df)
 
-x = tf.cast(df['LOR '].values, tf.float32)
-y = tf.cast(df['CGPA'].values, tf.float32)
+df.columns = ["No", "GRE", "TOEFL", "UR", "SOP", "LOR", "CGPA", "RES", "CoA", "RACE", "SES"]
 
-linear = LinearRegression()
+#print(df.describe())
+X = df[['GRE', 'TOEFL', 'CGPA']]
+y = df['CoA']
 
-epochs = 1000
-for epoch in range(epochs):
-  modelLoss = loss(y, linear(x))
-  train(linear, x, y)
-  finalWeight = linear.w0
-  finalBias = linear.bias
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1)
 
-print("LOSS: " + str(modelLoss.numpy()))
-print("WEIGHT: " + str(linear.w0.numpy()))
-print("BIAS: " + str(linear.bias.numpy()))
+# Standardizing the features:
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# Chose 0.82 because it is the 3rd quartile for chance of admit
+ty_train=[1 if CoA > 0.82 else 0 for CoA in y_train] # learned from internet
+ty_train=np.array(ty_train)
+
+ty_test=[1 if CoA > 0.82 else 0 for CoA in y_test] #learned from internet
+ty_test=np.array(ty_test)
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_train_std, ty_train)
+lin_pred = lin_reg.predict(X_test_std)
+print("Logistic Regression Accuracy: %.3f" % accuracy_score(ty_test, lin_pred))
+# Logistic Regression Accuracy: 0.958
 
 
-for i in range(len(x)):
 
-    if (df.iloc[i]['Chance of Admit '] == 1):
-        plt.plot(x[i],y[i], 'x', color='red', label='Admitted')
-    else:
-        plt.plot(x[i],y[i], 'o', color='green', label='not admitted')
-
-plt.plot(x, finalBias + finalWeight*x, color='blue')
-
-#axis
-plt.ylabel('age')
-plt.xlabel('time')
-
-plt.show()
-plt.show()
 
