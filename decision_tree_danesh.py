ff6f78086d20804cec0945f7c69e3f5f387e5c73
Sam Bluestone <sambluestone@outlook.com>
2020-10-18 22:09:58 -0400

    decision regions for decision tree and random forest

--- decision_tree_danesh.py
+++ decision_tree_danesh.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt 
+from matplotlib.colors import ListedColormap
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
@@ -18,6 +19,53 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    classifier.fit(X, y)
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='test set')
+
+
+
+
 
 #read the data into a pandas dataframe
 df = pd.read_csv('Admission_Predict.csv')
@@ -53,6 +101,12 @@ dot_data = export_graphviz(tree_model,
 graph = graph_from_dot_data(dot_data)
 
 graph.write_png("tree.png")
+
+X_combined_std = np.vstack((X_train[['CGPA', 'TOEFL']], X_test[['CGPA', 'TOEFL']]))
+y_combined = np.hstack((ty_train, ty_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=DecisionTreeClassifier(criterion='gini', max_depth=2, random_state=1))
+plt.savefig("decision_tree.png")
+plt.show()
 
 print("Decision Tree Accuracy: %.3f" % accuracy_score(ty_test, dt_pred))
 print("Decision Tree F1-Score: %.3f" % f1_score(ty_test, dt_pred))
--- random_forest.py
+++ random_forest.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt 
+from matplotlib.colors import ListedColormap
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
@@ -16,6 +17,54 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    classifier.fit(X, y)
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='test set')
+
+#read the data into a pandas dataframe
+df = pd.read_csv('Admission_Predict.csv')
+df.dropna(inplace=True)
+
 
 #read the data into a pandas dataframe
 df = pd.read_csv('Admission_Predict.csv')
@@ -41,6 +90,14 @@ ty_test=np.array(ty_test)
 forest = RandomForestClassifier(criterion='gini', n_estimators=25, random_state=1, n_jobs=2)
 forest.fit(X_train, ty_train)
 rf_pred = forest.predict(X_test)
+
+
+X_combined_std = np.vstack((X_train[['CGPA', 'TOEFL']], X_test[['CGPA', 'TOEFL']]))
+y_combined = np.hstack((ty_train, ty_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=RandomForestClassifier(criterion='gini', n_estimators=25, random_state=1, n_jobs=2))
+plt.savefig("random_forest.png")
+plt.show()
+
 print("Random Forest Accuracy: %.3f" % accuracy_score(ty_test, rf_pred))
 print("Random Forest F1-Score: %.3f" % f1_score(ty_test, rf_pred))
 print("Random Forest Precision: %.3f" % precision_score(ty_test, rf_pred))
