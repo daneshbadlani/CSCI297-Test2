0912a5d5336e47706406cfa36c7ab2aff339499a
Sam Bluestone <sambluestone@outlook.com>
2020-10-18 21:06:10 -0400

    added decision region graph for logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt 
+from matplotlib.colors import ListedColormap
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
@@ -16,6 +17,50 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    classifier.fit(X, y)
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='test set')
+
+
 
 #read the data into a pandas dataframe
 df = pd.read_csv('Admission_Predict.csv')
@@ -55,3 +100,9 @@ print("Logistic Regression Recall: %.3f" % recall_score(ty_test, log_pred))
 # Logistic Regression F1-Score: 0.941
 # Logistic Regression Precision: 0.960
 # Logistic Regression Recall: 0.923
+
+X_combined_std = np.vstack((X_train_std[:, 1:], X_test_std[:, 1:]))
+y_combined = np.hstack((ty_train, ty_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=LogisticRegression(C=100, random_state=1, solver='lbfgs', multi_class='ovr'))
+plt.savefig("log_reg.png")
+plt.show()
