bcc87a05613a648c417abfb34edb3a6a9f7da6cf
Sam Bluestone <sambluestone@outlook.com>
2020-10-18 21:34:53 -0400

    decision regions for svm

--- SVM.py
+++ SVM.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt 
+from matplotlib.colors import ListedColormap
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
@@ -16,6 +17,48 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    classifier.fit(X, y)
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='test set')
 
 #read the data into a pandas dataframe
 df = pd.read_csv('Admission_Predict.csv')
@@ -47,6 +90,15 @@ ty_test=np.array(ty_test)
 svm = SVC(kernel='linear', C=2.0, random_state=1)
 svm.fit(X_train_std, ty_train)
 svc_pred = svm.predict(X_test_std)
+
+
+X_combined_std = np.vstack((X_train_std[:, 1:], X_test_std[:, 1:]))
+y_combined = np.hstack((ty_train, ty_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=SVC(kernel='linear', C=2.0, random_state=1))
+plt.savefig("svm.png")
+plt.show()
+
+
 print("SVM Accuracy: %.3f" % accuracy_score(ty_test, svc_pred))
 print("SVM F1-Score: %.3f" % f1_score(ty_test, svc_pred))
 print("SVM Precision: %.3f" % precision_score(ty_test, svc_pred))
